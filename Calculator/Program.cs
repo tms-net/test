fc3593062d8a099c5ec96f06d5942b9d1e6ebf58
Dmitry <diman.84@gmail.com>
2022-04-14 19:30:36 +0300

    initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            while (true)
+            {
+                double a, b;
+                char operation;
+
+                try
+                {
+                    Console.WriteLine("Введите первое число");
+                    a = Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Введите число!");
+                    continue;
+                }
+
+                Console.WriteLine("Укажите операцию");
+                operation = Convert.ToChar(Console.ReadLine());
+                switch (operation)
+                {
+
+                    case '+':
+                        break;
+
+                    case '-':
+                        break;
+
+                    case '*':
+                        break;
+
+                    case '/':
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown operation!");
+                        continue;
+
+                }
+
+                try
+                {
+                    Console.WriteLine("Введите второе число");
+                    b = Convert.ToDouble(Console.ReadLine());
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите число!");
+                    continue;
+                }
+
+
+
+                if (operation == '+')
+                {
+                    Console.WriteLine("Результат = " + (a + b));
+                }
+                else if (operation == '-')
+                {
+                    Console.WriteLine("Результат = " + (a - b));
+                }
+                else if (operation == '*')
+                {
+                    Console.WriteLine("Результат = " + (a * b));
+                }
+                else if (operation == '/')
+                {
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Делить на 0 нельзя!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Результат = " + (a / b));
+                    }
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ FirstConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyName
+{
+    class FirstName
+    {
+        static void Main(string[] args)
+        {
+            string name, surname;
+
+            Console.WriteLine("Ваше имя:");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Ваша фамилия:");
+
+            surname = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Hello " + name + " " + surname + "!");
+        }
+    }
+}
+
+
